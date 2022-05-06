0e15a67a94cd07f4af2303605b1b2f394b8ed507
y.kang@rochester.edu <y.kang@rochester.edu>
2022-05-06 11:11:09 +0000

    application with duplicate tokens

--- 06 Token Recommender.py
+++ 06 Token Recommender.py
@@ -27,9 +27,44 @@ print(wallet_address,start_date)
 
 # COMMAND ----------
 
+##Currently Everything is selected to compare the name and symbol, for the actual application, comment out the select all and uncomment the top line. 
+rec = spark.sql("""
+-- SELECT g04_db.toprecs_one_user.name, g04_db.toprecs_one_user.symbol, image, contract_address
+SELECT *
+FROM g04_db.toprecs_one_user
+LEFT JOIN ethereumetl.token_prices_usd
+ON UPPER(g04_db.toprecs_one_user.symbol) = UPPER(ethereumetl.token_prices_usd.symbol) 
+-- AND UPPER(g04_db.toprecs_one_user.name) = UPPER(ethereumetl.token_prices_usd.name) 
+LIMIT 10
+""").toPandas()
 
+rec
+##uncomment to see the application
+# results = ""
+# if(rec.empty):
+#     displayHTML("""<h2>Error: Recommendation Empty</h2>""")
+# else:
+#     for index, row in rec.iterrows():
+#         results += """
+#         <tr>
+#           <td style="padding:.5rem"><img src="{2}"></td>          
+#           <td style="padding:.5rem">{0} ({1})</td>          
+#           <td style="padding:.5rem"> <a href="https://etherscan.io/address/{3}"> <img src="https://freeiconshop.com/wp-content/uploads/edd/link-closed-flat.png" width="25" height="25"> </a> </td>
+#         </tr>
+#         """.format(row["name"], row["symbol"], row["image"] , row["contract_address"])
+    
+#     displayHTML("""
+#         <h2>Recommend Tokens for user address:</h2>
+#         <table style="padding:.5rem, border:0">
+#         {0}
+#         </table>
+#     """.format(results))
 
 # COMMAND ----------
 
 # Return Success
 dbutils.notebook.exit(json.dumps({"exit_code": "OK"}))
+
+# COMMAND ----------
+
+
